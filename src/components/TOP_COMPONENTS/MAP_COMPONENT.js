f329b4d86ee2b913a2731fe69726dc9d5dc15302
Fraser <58369886+FMuir@users.noreply.github.com>
2022-06-10 22:16:05 -0700

    work on embedding google map

--- src/components/TOP_COMPONENTS/MAP_COMPONENT.js
+++ src/components/TOP_COMPONENTS/MAP_COMPONENT.js
@@ -1,3 +1,16 @@
-export default function MAP_COMPONENT(){
-    return (<h1>Placeholder for Map API</h1>);
+import {useEffect, useRef, useState} from "react";
+
+
+export default function MAP_COMPONENT({center, zoom}){
+
+    const ref = useRef(null);
+    const [map, setMap] = useState();
+
+    useEffect(() => {
+        if (ref.current && !map) {
+            setMap(new window.google.maps.Map(ref.current, {}));
+        }
+    }, [ref, map]);
+    //return <div ref={ref} id={"map"} />;
+
 }
--- src/components/TOP_COMPONENTS/RIGHT_TOP_CONTAINER.js
+++ src/components/TOP_COMPONENTS/RIGHT_TOP_CONTAINER.js
@@ -1,4 +1,23 @@
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import MAP_COMPONENT from "./MAP_COMPONENT";
 export default function RIGHT_TOP_CONTAINER(){
+    // const API_KEY = ProcessInfo.processInfo.environment["API_KEY"];
+    const center = {lat: 49.2606, lng: 123.2460};
+    const zoom = 7;
+    const render = (status) => {
+        switch (status) {
+            case Status.LOADING:
+                return <h1>Loading</h1>;
+            case Status.FAILURE:
+                return <h1>Failure</h1>;
+            case Status.SUCCESS:
+                return <MAP_COMPONENT center={center} zoom={zoom} />;
+        }
+    };
+
+    return (
+    <Wrapper apiKey={""} render={render} />
+    );
+
     return (<span id="RIGHT_TOP" style={{"display":"inline-block"}}><MAP_COMPONENT/></span>);
 }
