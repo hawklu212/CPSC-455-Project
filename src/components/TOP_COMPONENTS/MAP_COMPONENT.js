5ab5f004a17a54292076c3234a585329618f592d
Fraser <58369886+FMuir@users.noreply.github.com>
2022-06-11 00:03:55 -0700

    Got embedded map working

--- src/components/TOP_COMPONENTS/MAP_COMPONENT.js
+++ src/components/TOP_COMPONENTS/MAP_COMPONENT.js
@@ -1,16 +1,17 @@
 import {useEffect, useRef, useState} from "react";
 
 
-export default function MAP_COMPONENT({center, zoom}){
-
+export default function MAP_COMPONENT({center, zoom}) {
     const ref = useRef(null);
     const [map, setMap] = useState();
 
     useEffect(() => {
         if (ref.current && !map) {
-            setMap(new window.google.maps.Map(ref.current, {}));
+            setMap(new window.google.maps.Map(ref.current, {center, zoom}));
         }
     }, [ref, map]);
-    //return <div ref={ref} id={"map"} />;
+
+    console.log(ref)
+    return <div ref={ref} id={"map"} style={{display: "inline-flex", height: `500px`, width: `500px`}}/>;
 
 }
--- src/components/TOP_COMPONENTS/TOP_DIV.js
+++ src/components/TOP_COMPONENTS/TOP_DIV.js
@@ -1,5 +1,8 @@
 import LEFT_TOP_CONTAINER from "./LEFT_TOP_CONTAINER"
 import RIGHT_TOP_CONTAINER from "./RIGHT_TOP_CONTAINER"
 export default function TOP_DIV(){
-    return (<div id="TOP_DIV"><LEFT_TOP_CONTAINER/><RIGHT_TOP_CONTAINER/></div>);
+    return (<div id="TOP_DIV">
+        <LEFT_TOP_CONTAINER/>
+        <RIGHT_TOP_CONTAINER/>
+    </div>);
 }
