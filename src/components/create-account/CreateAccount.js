52ad3401ab1f7d41b914c51accb53987b5ea4a19
justinjao <jao.c.justin@gmail.com>
2022-06-11 02:14:56 -0700

    added forms to Login and CreateAccount pages

--- src/components/create-account/CreateAccount.js
+++ src/components/create-account/CreateAccount.js
@@ -1,1 +1,31 @@
-export default function CreateAccount() {}
+import { Grid, Typography, Button, Divider } from "@mui/material";
+import "../../components_styling/colours.css";
+import { TextField } from "@mui/material";
+
+export default function CreateAccount() {
+  return (
+    <Grid
+      className="yellow-2"
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Typography variant="h2">A11yMaps</Typography>
+      <br />
+      <br />
+      <br />
+      <Typography variant="h4">Hi there! We're here to help</Typography>
+      <br />
+      <TextField variant="filled" label="Username"></TextField>
+      <br />
+      <TextField label="Password"></TextField>
+      <br />
+      <span>
+        <Button variant="outlined">Sign in</Button>
+      </span>
+    </Grid>
+  );
+}
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,3 +1,38 @@
+import { Grid, Typography, Button, Divider } from "@mui/material";
+import "../../components_styling/colours.css";
+import { TextField } from "@mui/material";
+
 export default function Login() {
-  return <div></div>;
+  return (
+    <Grid
+      className="yellow-2"
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Typography variant="h2">A11yMaps</Typography>
+      <br />
+      <br />
+      <br />
+      <Typography variant="h4">Welcome Back!</Typography>
+      <br />
+      <TextField variant="filled" label="Username"></TextField>
+      <br />
+      <TextField label="Password"></TextField>
+      <br />
+      <span>
+        <Button variant="outlined">Sign in</Button>
+      </span>
+      <br />
+      <br />
+      <br />
+      <Divider></Divider>
+      <Typography variant="h6">Don't have an account? Sign up here!</Typography>
+      <br />
+      <Button variant="outlined">Sign up</Button>
+    </Grid>
+  );
 }
