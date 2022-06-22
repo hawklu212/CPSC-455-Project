1fd859973bb628ddd9bb020ec38b548b78ed1d03
justinjao <jao.c.justin@gmail.com>
2022-06-21 22:17:21 -0700

    fixed up drawer

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,10 +7,17 @@ import {
   Toolbar,
   IconButton,
   Drawer,
+  Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemButton,
+  ListItemText,
 } from "@mui/material";
 import "../components-styling/colours.css";
 import { useNavigate } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
+import NordicWalkingIcon from "@mui/icons-material/NordicWalking";
 import { useState } from "react";
 
 export default function Navigation() {
@@ -22,6 +29,41 @@ export default function Navigation() {
     setDrawerState(!drawerState);
   };
 
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer}
+      onKeyDown={toggleDrawer}
+    >
+      <List>
+        {["Maps", "Saved Routes"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MenuIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      {/* <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MenuIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List> */}
+    </Box>
+  );
+
   return (
     <AppBar position="static">
       <Toolbar variant="dense">
@@ -30,11 +72,12 @@ export default function Navigation() {
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
+          onClick={toggleDrawer}
         >
-          <MenuIcon onClick={toggleDrawer} />
+          <MenuIcon />
         </IconButton>
         <Drawer anchor="left" open={drawerState} onClose={toggleDrawer}>
-          Haha
+          {list()}
         </Drawer>
         <Typography variant="h6" color="inherit" component="div">
           A11yMaps
