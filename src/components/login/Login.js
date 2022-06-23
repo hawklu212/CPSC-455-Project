94f6729ca48da7380970b1db5ee1eac502b3087a
unknown <hawklu212@hotmail.com>
2022-06-23 11:45:06 -0700

    added base validation for missing fields

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,12 +4,17 @@ import { TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Navigation from "../Navigation";
 import { useDispatch, useSelector } from "react-redux/es/exports";
+import { useState } from "react";
 
 var userName="";
 var userPass="";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [userError,setuserError]=useState(false);
+  const [passError,setpassError]=useState(false);
+  const [userErrorMessage,setuserErrorMessage]=useState("");
+  const [passErrorMessage,setpassErrorMessage]=useState("");
 
   const loginAttempt = () => {
     //TODO: add authentication (maybe following this? https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications)
@@ -37,19 +42,34 @@ export default function Login() {
       <br />
       <Typography variant="h4">Welcome Back!</Typography>
       <br />
-      <TextField variant="filled" label="Username" onChange={(event)=>
+      <TextField error={userError} helperText={userErrorMessage} required variant="filled" label="Username" onChange={(event)=>
       {userName=event.target.value;
         console.log(userName);
       }}></TextField>
       <br />
-      <TextField label="Password" type="password" onChange={(event)=>
+      <TextField error={passError} helperText={passErrorMessage} required label="Password" type="password" onChange={(event)=>
         {userPass=event.target.value;
           console.log(userPass);
         }}></TextField>
       <br />
       <span>
         <Button variant="outlined" onClick={()=>{
-          loginAttempt();
+          let errorMSG =data =>{return `Missing ${data}`};
+          if (userPass==="" && userName===""){
+            setpassError(true);
+            setuserError(true);
+            setpassErrorMessage(errorMSG("Password"));
+            setuserErrorMessage(errorMSG("Username"));            
+          }
+          else if (userPass===""){
+            setpassError(true);
+            setpassErrorMessage(errorMSG("Password"));
+          }
+          else if (userName===""){
+            setuserError(true);
+            setuserErrorMessage(errorMSG("Username"));
+          }
+          //loginAttempt();
         }}>Sign in</Button>
       </span>
       <br />
