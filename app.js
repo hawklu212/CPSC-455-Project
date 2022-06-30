2c4d2517388a3dbb23615294cfaad3c1f48eaf78
unknown <hawklu212@hotmail.com>
2022-06-30 01:47:42 -0700

    figured the initial cookie setup

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000',"http://localhost:3000"],credentials:true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
