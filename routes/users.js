8333688bd0cb0ec31f634c83c80afed6847af2d9
unknown <hawklu212@hotmail.com>
2022-06-30 13:52:13 -0700

    secured credential login

--- routes/users.js
+++ routes/users.js
@@ -5,61 +5,75 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var loginSchema = new Schema({
   userName: String,
-  userPass: String
+  userPass: String,
+  accessToken: String
 });
+var LoginModel = mongoose.model('LoginModel', loginSchema );
+
+let user="userName";
+let pass="userPass";
+
 
 function valCookie(req,res,next){
     const {cookies}=req;
     if ("session_id" in cookies){
         console.log("session_id exists");
-        if (cookies.session_id=="123456"){
-            next();
-        }else{
-            console.log("your mom1");
-            res.status(403).send({"msg":"cookie not valid"});
-        }
+        next();
     } else{
         console.log("your mom2");
         res.status(403).send({"msg":"no session present"});
 }
 }
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.cookie("session_id", 123456);
-  res.status(200).send({"msg":"Logged in"});
+/* GET users listing. Initial Login */ 
+router.put('/cookie', function(req, res, next) {
+  let curr=req.body[user];
+  LoginModel.find({accessToken:curr}).then(arr=>{
+    if (arr.length!==0){
+        const test = arr[0][user];
+        res.cookie("session_id", "123456");
+        res.send(getReturnHelper(0,test,arr[0],"")).status(304);
+    }else{
+        res.send(getReturnHelper(1,"","")).status(403);
+    }
+  });
 });
 
 router.get('/cookie',valCookie ,function(req, res) {
-  res.status(200).send({"msg":"Logged in", "userName":"Justin"});
+  const {cookies}=req;
+  LoginModel.find({accessToken:cookies.session_id}).then(ref=>{
+    if (ref.length!==0){
+        console.log(ref[0][user]);
+        res.status(200).send({[user]:ref[0][user]});
+    }else{
+        console.log("your mom1");
+        res.status(403).send({"msg":"cookie not valid"});
+    }
+  })
 });
 
 
 
-function getReturnHelper(stat,payload){
-    return {"status":stat,"userName":payload};
+function getReturnHelper(stat,payload,token){
+    return {"status":stat,"userName":payload,"accessToken":token};
 }
-
-let user="userName";
-let pass="userPass";
 
 /* GET users listing. */
 router.put('/', function(req, res, next) {
     let name=req.body[user];
-    var LoginModel = mongoose.model('LoginModel', loginSchema );
     LoginModel.find({userName:name}).then((arr)=>{
         if (arr.length!==0){
             console.log(arr[0]);
             console.log(req.body[pass]);
             if (arr[0][pass]===req.body[pass]){
-                res.send(getReturnHelper(0,name));
+                res.cookie("session_id", "123456").send(getReturnHelper(0,name,arr[0]["accessToken"]));
             } else{
-                res.send(getReturnHelper(2,""));
+                res.send(getReturnHelper(2,"",""));
                 return;
             }
         }
         else {
-            res.send(getReturnHelper(1,""));
+            res.send(getReturnHelper(1,"",""));
             return;
         }
     })
@@ -71,9 +85,9 @@ router.post('/', function(req, res, next) {
     var LoginModel = mongoose.model('LoginModel', loginSchema );
     LoginModel.find({userName:name}).then((arr)=>{
         if (arr.length==0){
-            const test = new LoginModel({ userName: name,userPass:req.body[pass] });
+            const test = new LoginModel({ userName: name,userPass:req.body[pass],accessToken:"123456" });
             test.save().then(stuff=>{
-                res.send(getReturnHelper(0,name));
+                res.send(getReturnHelper(0,name,"123456"));
                 console.log(stuff);
             })
         }
