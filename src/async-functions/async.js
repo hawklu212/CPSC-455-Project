2a09364ca6684e0013406e7a8943e5238e8d2918
unknown <hawklu212@hotmail.com>
2022-06-30 01:47:42 -0700

    figured the initial cookie setup

--- src/async-functions/async.js
+++ src/async-functions/async.js
@@ -31,3 +31,37 @@ export async function loginCurl(data){
       });
         return response.json(); // parses JSON response into native JavaScript objects
       }
+
+      export async function getCookieCurl(){
+        let response= await fetch("http://localhost:3001/users",{
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'include', // include, *same-origin, omit
+            headers: {
+              'Content-Type': 'application/json',
+              //"Access-Control-Allow-Origin": "http://localhost:3001*",
+              //"Access-Control-Allow-Credentials": true
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        });
+          return response.status; // parses JSON response into native JavaScript objects
+        }      
+
+        export async function getCookieValidationCurl(){
+          let response= await fetch("http://localhost:3001/users/cookie",{
+              method: 'GET', // *GET, POST, PUT, DELETE, etc.
+              mode: 'cors', // no-cors, *cors, same-origin
+              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+              credentials: 'include', // include, *same-origin, omit
+              headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              redirect: 'follow', // manual, *follow, error
+              referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          });
+            return response; // parses JSON response into native JavaScript objects
+          }      
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,25 +2,37 @@ import Title from "./TitleDiv";
 import TopDiv from "./top-components/TopDiv";
 import BottomDiv from "./bottom-components/BottomDiv";
 import Navigation from "./Navigation";
-import { useEffect } from "react";
+import { useEffect ,useMemo} from "react";
 import { loginState } from "../actions";
 import { useDispatch, useSelector } from "react-redux";
+import { getCookieCurl, getCookieValidationCurl } from "../async-functions/async";
+import { useNavigate } from "react-router-dom";
 export default function MainContainer() {
+  const navigate=useNavigate()
   const dispatch=useDispatch();
   const loginUser=useSelector(state=>state.loginState);
   useEffect(() => {
     if(loginUser!==""){
-      localStorage.setItem("user",loginUser);
-      return;
-    }
-    const loggedInUser = localStorage.getItem("user");
-    if (loggedInUser!== "undefined") {
-      dispatch(loginState(loggedInUser));
+      getCookieCurl().then(status=>{
+        if (status!=200){
+          navigate("../");
+          return;
+        }
+      });
     } else{
-      dispatch(loginState("None"));
+      getCookieValidationCurl().then(res=>{
+        if (res.status!=200){
+          navigate("../");
+          return;
+        } else{
+          res.json().then(ret=>{
+            dispatch(loginState(ret["userName"]));
+          });
+        }
+      });
     }
   }, []);
-  return (
+  return loginUser!==""? (
     <div className="yellow-2"
          container
          spacing={0}
@@ -37,5 +49,5 @@ export default function MainContainer() {
         <BottomDiv />
       </div>
     </div>
-  );
+  ):"";
 }
