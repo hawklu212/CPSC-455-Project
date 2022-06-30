434267a4813dfd0162c59ea5b8fa4622c7584fbc
unknown <hawklu212@hotmail.com>
2022-06-30 01:47:42 -0700

    figured the initial cookie setup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var loginSchema = new Schema({
@@ -11,10 +8,32 @@ var loginSchema = new Schema({
   userPass: String
 });
 
+function valCookie(req,res,next){
+    const {cookies}=req;
+    if ("session_id" in cookies){
+        console.log("session_id exists");
+        if (cookies.session_id=="123456"){
+            next();
+        }else{
+            console.log("your mom1");
+            res.status(403).send({"msg":"cookie not valid"});
+        }
+    } else{
+        console.log("your mom2");
+        res.status(403).send({"msg":"no session present"});
+}
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.cookie("session_id", 123456);
+  res.status(200).send({"msg":"Logged in"});
 });
+
+router.get('/cookie',valCookie ,function(req, res) {
+  res.status(200).send({"msg":"Logged in", "userName":"Justin"});
+});
+
 
 
 function getReturnHelper(stat,payload){
