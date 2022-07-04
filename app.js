79d99bbb7f2349de8130069a7f117a947c2ef00f
unknown <hawklu212@hotmail.com>
2022-07-03 21:26:36 -0700

    removed login.js trace

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cors= require('cors');
 var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
 var mongoDB = 'mongodb://127.0.0.1/Test2';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -34,7 +33,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
