d44dae63a1dafb3c0e17c7c4af3f3467a50283cf
justinjao <jao.c.justin@gmail.com>
2022-07-07 22:48:19 -0700

    added file to be used for ranking all the routes

---
+++ src/server/RouteRankingService.js
@@ -0,0 +1,9 @@
+// this file can hold functions to process ranking the routes from
+// routeResultArray. Perhaps by assigning a ranking to the ranking member
+// of each route object, or by simply sorting the list
+
+// will need to depend on what user preferences are set as well
+// e.g. if distance more important than incline, then we sort by that?
+
+// we could perhaps even assign more ranking fields to the routeSummary object
+// based on how well each
