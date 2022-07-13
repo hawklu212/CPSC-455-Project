de457a0fb6c2af2524d79b52356bef00a128273b
Justin Jao <47490085+justinjao@users.noreply.github.com>
2022-07-13 00:00:36 -0700

    Jj/add sample requests (#43)
    * added nodeJS client dependency for maps api
    
    * WIP adding sample code for NodeJS maps client
    
    * added request to pass in location data to backend
    
    * cleanup RightTopContainer code
    
    * WIP adding directionsRequest
    
    * updated gitignore to include apiKeys
    
    * added more wip for directions
    
    * WIP adding directions requests to endpoints
    
    * fixed get directions with API key and TravelMode bug
    
    * wip adding client elevation requests
    
    * Added sample request for viewing
    
    * added WIP elevation requests and comments of TODO
    
    * added elevation request to google maps api
    
    * Added more skeleton for route response data back to frontend
    
    * added successful elevation requests and passing back of info to frontend
    
    * Added file to add functions for route processing
    
    * added file to be used for ranking all the routes

--- src/async-functions/async.js
+++ src/async-functions/async.js
@@ -1,87 +1,104 @@
-var endpoint="http://localhost:3001/"
-export async function loginCurl(data){
-    let response= await fetch(`${endpoint}users`,{
-        method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data)
-    });
-      return response.json(); // parses JSON response into native JavaScript objects
-    }
+/* eslint-disable require-jsdoc */
+const endpoint = "http://localhost:3001/";
 
-    export async function signUpCurl(data){
-      let response= await fetch(`${endpoint}users`,{
-          method: 'POST', // *GET, POST, PUT, DELETE, etc.
-          mode: 'cors', // no-cors, *cors, same-origin
-          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: 'same-origin', // include, *same-origin, omit
-          headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          redirect: 'follow', // manual, *follow, error
-          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-          body: JSON.stringify(data)
-      });
-        return response.json(); // parses JSON response into native JavaScript objects
-      }
+export async function loginCurl(data) {
+  const response = await fetch(`${endpoint}users`, {
+    method: "PUT", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data),
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
-      export async function setInitialCookieCurl(data){
-        let response= await fetch(`${endpoint}users/cookie`,{
-            method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'include', // include, *same-origin, omit
-            headers: {
-              'Content-Type': 'application/json',
-              //"Access-Control-Allow-Origin": "http://localhost:3001*",
-              //"Access-Control-Allow-Credentials": true
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body:JSON.stringify(data)
-          });
-          return response.json(); // parses JSON response into native JavaScript objects
-        }      
+export async function signUpCurl(data) {
+  const response = await fetch(`${endpoint}users`, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data),
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
-        export async function getCookieValidationCurl(){
-          let response= await fetch(`${endpoint}users/cookie`,{
-              method: 'GET', // *GET, POST, PUT, DELETE, etc.
-              mode: 'cors', // no-cors, *cors, same-origin
-              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-              credentials: 'include', // include, *same-origin, omit
-              headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-              },
-              redirect: 'follow', // manual, *follow, error
-              referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-          });
-            return response; // parses JSON response into native JavaScript objects
-          }      
+export async function setInitialCookieCurl(data) {
+  const response = await fetch(`${endpoint}users/cookie`, {
+    method: "PUT", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "include", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // "Access-Control-Allow-Origin": "http://localhost:3001*",
+      // "Access-Control-Allow-Credentials": true
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data),
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
-          export async function logoutCurl(){
-            let response= await fetch(`${endpoint}users/cookie/logout`,{
-                method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'include', // include, *same-origin, omit
-                headers: {
-                  'Content-Type': 'application/json',
-                  //"Access-Control-Allow-Origin": "http://localhost:3001*",
-                  //"Access-Control-Allow-Credentials": true
-                  // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-              });
-              return response; // parses JSON response into native JavaScript objects
-            } 
+export async function getCookieValidationCurl() {
+  const response = await fetch(`${endpoint}users/cookie`, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "include", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
+  return response; // parses JSON response into native JavaScript objects
+}
+
+export async function logoutCurl() {
+  const response = await fetch(`${endpoint}users/cookie/logout`, {
+    method: "PUT", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "include", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // "Access-Control-Allow-Origin": "http://localhost:3001*",
+      // "Access-Control-Allow-Credentials": true
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
+  return response; // parses JSON response into native JavaScript objects
+}
+
+// JJ: at some point, may need to pass in user here as well
+export async function getRouteResults(locations) {
+  const routeParams = new URLSearchParams({
+    orig: locations[0],
+    dest: locations[1],
+  });
+  const response = await fetch(`${endpoint}routes/?` + routeParams, {
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+  });
+  return response.json();
+}
