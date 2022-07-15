a5a65995b80d2858935a8059c9561be096183275
Fraser Muir <fraser@Frasers-MBP.lan>
2022-07-15 13:56:58 -0400

    modified profileSchema

--- src/server/database/profileSchema.js
+++ src/server/database/profileSchema.js
@@ -2,11 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const profileSchema = new Schema({
-    email: String,
-    userName: String,
-    userPass: String,
-    accessToken: String,
-    verified: Boolean,
+    maxIncline: Number,
+    weight: Number,
+    distancePreference: String,
 });
 
 module.exports = mongoose.model('ProfileModel', profileSchema);
