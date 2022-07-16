a66781a419439308456bbb98fe408fa12977c087
Fraser <58369886+FMuir@users.noreply.github.com>
2022-07-16 01:09:11 -0700

    Fraser/add route rating (#59)
    * create profileSchema and ProfileModel
    
    * add comment on suitableElevationSampleSize
    
    * add a constant for number of samples to take from each step
    
    * updated values in profileSchema
    
    * modified profileSchema
    
    * refactor getElevationResults to return object with elevationDataArray and routeScore, and modified the usage of the method in routes.js to accomodate the change
    
    * implement calculateIncline and calculateStepScore
    
    * Incorporate elevation, incline, and route score results into routeSummary
    
    * incorporate calculating score method calls and make the necessary changes to include hte results in MapsClientService
    
    * added in placeholder user email for searching userProfile in the database
    
    * remove a TODO
    
    * add default value for userMass in case user hasn't set a weight in their profile
    
    * debugged and fixed all calculations
    
    Co-authored-by: Fraser Muir <fraser@Frasers-MacBook-Pro.local>
    Co-authored-by: Fraser Muir <fraser@Frasers-MBP.lan>

--- src/server/MapsClientService.js
+++ src/server/MapsClientService.js
@@ -1,5 +1,7 @@
 const { Client, TravelMode } = require("@googlemaps/google-maps-services-js");
 const APIKey = require("./apiKeyExpress");
+const {calculateStepScore} = require("./RouteProcessingService");
+const ProfileModel = require("./database/profileSchema")
 
 const client = new Client({});
 
@@ -21,34 +23,56 @@ const getDirectionsResults = async (orig, dest, waypoints) => {
 // Note: elevation API can consume multiple types. For now, let's either pass in
 // an address, or latitude and longitude
 const getElevationResults = async (route) => {
+  // TODO: change this after testing
+  const numberOfSamples = 10;
   // assuming we only have 1 leg for now, and we aren't using waypoints
   let legSteps = route.legs[0].steps;
 
-  let elevationDataArray = [];
+  let elevationResults = {
+    elevationDataArray: [],
+    routeScore: 0,
+    steepestIncline: null,
+  }
+
+  //let elevationDataArray = [];
 
   for (const step of legSteps) {
     let distanceOfStep = step.distance.value;
+    // This would give a value in meters, not number of samples I believe?
     let suitableElevationSampleSize = distanceOfStep / 10; // to start, let's just do 1/10
+    let subSampleDistance = distanceOfStep / numberOfSamples;
     let startLocation = [step.start_location.lat, step.start_location.lng];
     let endLocation = [step.end_location.lat, step.end_location.lng];
 
     let elevationRequest = {
       params: {
         path: [startLocation, endLocation],
-        samples: 10, // TODO: replace with ElevationSampleSize once done testing
+        samples: numberOfSamples, // TODO: replace with ElevationSampleSize once done testing
         key: APIKey.APIKey,
       },
       timeout: 1000,
     };
 
     let elevationData = await client.elevation(elevationRequest);
+    // TODO: address how to find the individuals profile - email address?
+
+    let emailAddress = "temp@gmail.com";
+    let userProfile = ProfileModel.find({email: emailAddress});
+
+    // calculateStepScore - take in elevationResults, elevationData, subSampleDistance and userProfile
+    // - return nothing, update score in elevationResults
+    console.log("before calculateStepScore line 64")
+    await calculateStepScore(elevationResults, elevationData.data.results, subSampleDistance, userProfile)
+    console.log(elevationResults);
+    console.log("after calculateStepScore line 66")
+
     elevationData.data.results.forEach((coordinate) => {
-      elevationDataArray.push(coordinate.elevation);
-      console.log(elevationDataArray);
+      elevationResults.elevationDataArray.push(coordinate.elevation);
+      // console.log(elevationResults.elevationDataArray);
     });
   }
 
-  return elevationDataArray;
+  return elevationResults;
 };
 
 module.exports = { getDirectionsResults, getElevationResults };
--- src/server/RouteProcessingService.js
+++ src/server/RouteProcessingService.js
@@ -5,9 +5,59 @@ const calculateTotalElevation = (routeSummary, elevationResults) => {
     Math.max(...elevationResults) - Math.min(...elevationResults);
 };
 
-// TODO: implement more calculation functions
-// const calculateSteepestIncline = (routeSummary, elevationResults) => {
+const calculateStepScore = async (elevationResults, elevationData, samplingDistance, userProfile) => {
+  // set a default value for mass if not weight is indicated in user profile?
+  let userMass = 60;
+  if (userProfile.weight != null) {
+    userMass = userProfile.weight;
+  }
+  console.log("userMass is" + userMass);
+  // TODO: incorporate distancePref into the calculation
+  // let distancePref = userProfile.distancePreference;
+  // TODO: use a better default rolling resistance
+  let rollingResistance = 1;
 
-// }
+  for (let i = 0; i < elevationData.length-2; i++) {
+    let incline = calculateIncline(elevationData[i].elevation, elevationData[i+1].elevation, samplingDistance);
+    console.log("after incline, incline is ")
+    console.log(incline);
+    // Set max incline if null, and update if slope is greater than the current steepestIncline
+    if ((elevationResults.steepestIncline === undefined) || elevationResults.steepestIncline < incline) {
+      elevationResults.steepestIncline = incline;
+    }
+    console.log("after steepest incline line 28")
 
-module.exports = { calculateTotalElevation };
+    // calculate the inclineFactor for this step
+    let inclineFactor = null;
+
+    if (incline < 0) {
+      inclineFactor = (userMass * samplingDistance * Math.tan(incline)) - (samplingDistance * rollingResistance);
+    } else {
+      inclineFactor = (userMass * samplingDistance * Math.tan(incline)) + (samplingDistance * rollingResistance);
+    }
+    console.log("line 38 after inclineFactor");
+    console.log(inclineFactor);
+
+    // We don't allow negatives for inclineFactor, so if negative set to zero
+    if (inclineFactor < 0) {
+      inclineFactor = 0;
+    }
+
+    // Calculate this step's score and add it to the current score in elevationResults
+    let stepScore = samplingDistance * inclineFactor;
+    elevationResults.routeScore += stepScore;
+    console.log(stepScore);
+    console.log(elevationResults.routeScore);
+  }
+
+}
+
+// return incline in degrees
+const calculateIncline = (startElevation, endElevation, distance) => {
+  let riseOverRun = (endElevation - startElevation) / distance;
+  let inclineRadians = Math.atan(riseOverRun);
+  let inclineDegrees = inclineRadians/180;
+  return inclineDegrees;
+}
+
+module.exports = { calculateTotalElevation, calculateStepScore };
