d8f0216284ae40ca7c056db374d2630cbbbe6c7b
unknown <hawklu212@hotmail.com>
2022-08-05 14:04:24 -0700

    added name feature for preventing duplicates

--- src/server/routes/savedRoutes.js
+++ src/server/routes/savedRoutes.js
@@ -26,14 +26,19 @@ router.get('/', valCookie,async function(req, res) {
     try{
       let { cookies } = req;
       let individual = await LoginModel.find({ accessToken: cookies.session_id });
-      let newSavedRoutes= new savedRouteModel({email: individual[0]["email"],origin:req.body["origin"],destionation:req.body["destination"]});
+      let duplicateCheck= await savedRouteModel.find({email: individual[0]["email"],name:req.body["name"]});
+      if (duplicateCheck.length!==0) {
+        res.send({exists:1});
+        return
+      }
+      let newSavedRoutes= new savedRouteModel({email: individual[0]["email"],origin:req.body["origin"],destination:req.body["destination"], name:req.body["name"]});
       await newSavedRoutes.save();
       let savedRoutes= await savedRouteModel.find({email: individual[0]["email"]});
       let arr=[];
       for (let i=0;i<savedRoutes.length;i++){
         arr.push(savedRoutes[i]);
       }
-      res.send(arr);
+      res.send({exists:0,results:arr});
       } catch(error){
         res.send({"error":error});
         return;
@@ -45,7 +50,7 @@ router.get('/', valCookie,async function(req, res) {
     try{
       let { cookies } = req;
       let individual = await LoginModel.find({ accessToken: cookies.session_id });
-      let savedRoutes= await savedRouteModel.deleteOne({email: individual[0]["email"],origin:req.body["origin"],destionation:req.body["destination"]});
+      let savedRoutes= await savedRouteModel.deleteOne({email: individual[0]["email"],origin:req.body["origin"],destionation:req.body["destination"],name:req.body["name"]});
       let newArr=await savedRouteModel.find({email: individual[0]["email"]});
 
       let arr=[];
