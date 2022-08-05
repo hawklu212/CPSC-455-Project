fe5493486c4fb4e695ec0081ea5cdc42dce83a69
unknown <hawklu212@hotmail.com>
2022-08-05 01:16:27 -0700

    added the delete, put, get function for savedRoute

--- src/server/routes/savedRoutes.js
+++ src/server/routes/savedRoutes.js
@@ -7,6 +7,7 @@ const savedRouteSchema = require("../database/savedRouteSchema");
 const savedRouteModel=savedRouteSchema;
 router.get('/', valCookie,async function(req, res) {
     try{
+      let { cookies } = req;
       let individual = await LoginModel.find({ accessToken: cookies.session_id });
       let savedRoutes= await savedRouteModel.find({email: individual[0]["email"]});
       let arr=[];
@@ -19,5 +20,44 @@ router.get('/', valCookie,async function(req, res) {
         return;
       }
   });
+
+
+  router.put('/', valCookie,async function(req, res) {
+    try{
+      let { cookies } = req;
+      let individual = await LoginModel.find({ accessToken: cookies.session_id });
+      let newSavedRoutes= new savedRouteModel({email: individual[0]["email"],origin:req.body["origin"],destionation:req.body["destination"]});
+      await newSavedRoutes.save();
+      let savedRoutes= await savedRouteModel.find({email: individual[0]["email"]});
+      let arr=[];
+      for (let i=0;i<savedRoutes.length;i++){
+        arr.push(savedRoutes[i]);
+      }
+      res.send(arr);
+      } catch(error){
+        res.send({"error":error});
+        return;
+      }
+  });
+
+
+  router.delete('/', valCookie,async function(req, res) {
+    try{
+      let { cookies } = req;
+      let individual = await LoginModel.find({ accessToken: cookies.session_id });
+      let savedRoutes= await savedRouteModel.deleteOne({email: individual[0]["email"],origin:req.body["origin"],destionation:req.body["destination"]});
+      let newArr=await savedRouteModel.find({email: individual[0]["email"]});
+
+      let arr=[];
+      for (let i=0;i<newArr.length;i++){
+      arr.push(newArr[i]);
+      }
+      res.send(arr);
+      } catch(error){
+        res.send({"error":error});
+        return;
+      }
+  });
+
 
 module.exports = router;
