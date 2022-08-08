7f00111127a2fc39c8e9f3a036003027aa785111
Justin Jao <47490085+justinjao@users.noreply.github.com>
2022-08-07 22:56:43 -0700

    Add saved routes (#75)
    * WIP adding saved routes for frontend
    
    * added WIP working implementation of adding new saved route
    
    * WIP fixed dropdown after adding new route
    
    * added delete all savedroutes
    
    * add saved route display to chips
    
    * finished implementation of saved routes
    
    * reverted endpoint

--- routes/savedRoutes.js
+++ routes/savedRoutes.js
@@ -2,67 +2,101 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const { valCookie } = require("./users");
+const { LoginModel } = require("./users");
 const savedRouteSchema = require("../database/savedRouteSchema");
 
-const savedRouteModel=savedRouteSchema;
-router.get('/', valCookie,async function(req, res) {
-    try{
-      let { cookies } = req;
-      let individual = await LoginModel.find({ accessToken: cookies.session_id });
-      let savedRoutes= await savedRouteModel.find({email: individual[0]["email"]});
-      let arr=[];
-      for (let i=0;i<savedRoutes.length;i++){
-        arr.push(savedRoutes[i]);
-      }
-      res.send(arr);
-      } catch(error){
-        res.send({"error":error});
-        return;
-      }
-  });
+const savedRouteModel = savedRouteSchema;
+router.get("/", valCookie, async function (req, res) {
+  try {
+    let { cookies } = req;
+    let individual = await LoginModel.find({ accessToken: cookies.session_id });
+    let savedRoutes = await savedRouteModel.find({
+      email: individual[0]["email"],
+    });
+    let arr = [];
+    for (let i = 0; i < savedRoutes.length; i++) {
+      arr.push(savedRoutes[i]);
+    }
+    res.send(arr);
+  } catch (error) {
+    res.send({ error: error });
+    return;
+  }
+});
 
+router.post("/", valCookie, async function (req, res) {
+  try {
+    let { cookies } = req;
+    let individual = await LoginModel.find({ accessToken: cookies.session_id });
+    let duplicateCheck = await savedRouteModel.find({
+      email: individual[0]["email"],
+      name: req.body["name"],
+    });
+    if (duplicateCheck.length !== 0) {
+      res.send({ exists: 1 });
+      return;
+    }
+    let newSavedRoutes = new savedRouteModel({
+      email: individual[0]["email"],
+      origin: req.body["origin"],
+      destination: req.body["destination"],
+      name: req.body["name"],
+    });
+    await newSavedRoutes.save();
+    let savedRoutes = await savedRouteModel.find({
+      email: individual[0]["email"],
+    });
+    let arr = [];
+    for (let i = 0; i < savedRoutes.length; i++) {
+      arr.push(savedRoutes[i]);
+    }
+    res.send({ exists: 0, results: arr });
+  } catch (error) {
+    res.send({ error: error });
+    return;
+  }
+});
 
-  router.put('/', valCookie,async function(req, res) {
-    try{
-      let { cookies } = req;
-      let individual = await LoginModel.find({ accessToken: cookies.session_id });
-      let duplicateCheck= await savedRouteModel.find({email: individual[0]["email"],name:req.body["name"]});
-      if (duplicateCheck.length!==0) {
-        res.send({exists:1});
-        return
-      }
-      let newSavedRoutes= new savedRouteModel({email: individual[0]["email"],origin:req.body["origin"],destination:req.body["destination"], name:req.body["name"]});
-      await newSavedRoutes.save();
-      let savedRoutes= await savedRouteModel.find({email: individual[0]["email"]});
-      let arr=[];
-      for (let i=0;i<savedRoutes.length;i++){
-        arr.push(savedRoutes[i]);
-      }
-      res.send({exists:0,results:arr});
-      } catch(error){
-        res.send({"error":error});
-        return;
-      }
-  });
+router.put("/", valCookie, async function (req, res) {
+  try {
+    let { cookies } = req;
+    let individual = await LoginModel.find({ accessToken: cookies.session_id });
 
+    let savedRoutes = await savedRouteModel.deleteMany({
+      email: individual[0]["email"],
+    });
+    let arr = [];
+    for (let i = 0; i < savedRoutes.length; i++) {
+      arr.push(savedRoutes[i]);
+    }
+    res.send({ exists: 0, results: arr });
+  } catch (error) {
+    res.send({ error: error });
+    return;
+  }
+});
 
-  router.delete('/', valCookie,async function(req, res) {
-    try{
-      let { cookies } = req;
-      let individual = await LoginModel.find({ accessToken: cookies.session_id });
-      let savedRoutes= await savedRouteModel.deleteOne({email: individual[0]["email"],origin:req.body["origin"],destionation:req.body["destination"],name:req.body["name"]});
-      let newArr=await savedRouteModel.find({email: individual[0]["email"]});
+router.delete("/", valCookie, async function (req, res) {
+  try {
+    let { cookies } = req;
+    let individual = await LoginModel.find({ accessToken: cookies.session_id });
+    let savedRoutes = await savedRouteModel.deleteOne({
+      email: individual[0]["email"],
+      origin: req.body["origin"],
+      destionation: req.body["destination"],
+      name: req.body["name"],
+    });
+    let newArr = await savedRouteModel.find({ email: individual[0]["email"] });
 
-      let arr=[];
-      for (let i=0;i<newArr.length;i++){
+    let arr = [];
+    for (let i = 0; i < newArr.length; i++) {
       arr.push(newArr[i]);
-      }
-      res.send(arr);
-      } catch(error){
-        res.send({"error":error});
-        return;
-      }
-  });
-
+    }
+    res.send(arr);
+  } catch (error) {
+    res.send({ error: error });
+    return;
+  }
+});
 
 module.exports = router;
