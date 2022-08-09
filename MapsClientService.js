8b02e0acaba03cecafd4593caef3667b2c98f7c7
Fraser <58369886+FMuir@users.noreply.github.com>
2022-08-08 20:22:42 -0700

    hard code samples to 10

--- MapsClientService.js
+++ MapsClientService.js
@@ -36,7 +36,8 @@ const getElevationResults = async (route, userProfile) => {
 
   for (const step of legSteps) {
     let distanceOfStep = step.distance.value;
-    const numberOfSamples = Math.floor(distanceOfStep/20);
+    const numberOfSamples = 10;
+    // Math.floor(distanceOfStep/20);
     let subSampleDistance = distanceOfStep / numberOfSamples;
     let startLocation = [step.start_location.lat, step.start_location.lng];
     let endLocation = [step.end_location.lat, step.end_location.lng];
